9f2e65c6dc8cab7f8293051d889949d1b612fd4c
NRodriguez52219 <117922302+NRodriguez52219@users.noreply.github.com>
2023-04-23 04:50:19 -0400

    Initialize patch-1

---
+++ main.cpp
@@ -0,0 +1,51 @@
+# include <iostream>
+# include <thread>
+
+#include "TicTacToe_Game.h"
+#include "Snake_Game.h"
+
+int main() {
+
+    cout << " ===========================================================================\n";
+	cout << " ||                                                                       ||\n";
+	cout << " ||                 *TICTACTOE  /  SNAKE GAME*                            ||\n";
+	cout << " ||                                                                       ||\n";
+	cout << " ||                                                                       ||\n";
+	cout << " ||                                                                       ||\n";
+	cout << " ||                              *Menu*                                   ||\n";
+	cout << " ||									  ||\n";
+	cout << " ||                         -> 1. Play TicTacToe                          ||\n";
+	cout << " ||                         -> 2. Play Snake Game                         ||\n";
+	cout << " ||                         -> 3. Exit                                    ||\n";
+	cout << " ||				                                          ||\n";
+	cout << " ||                                                                       ||\n";
+	cout << " ===========================================================================\n";
+	
+	/**
+	* Add code to this file to allow the user to play Tic-tac-toe or the Snake Game
+	* The user may play either game by pressing 1 or 2
+	**/
+	int option;
+
+	cin >> option;
+
+	switch(option)
+	{
+        case 1:
+		TicTacToe_Game game;
+            	game.playGame();
+            break;
+        case 2:
+		SnakeGame snakeGame;
+            	snakeGame.StartGame();
+            break;
+        default:
+            cout << "Thanks for Playing!!!" <<endl;
+            break;
+	}
+
+
+
+
+	return 0;
+}
